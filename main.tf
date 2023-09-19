0f972e0ea4ec8a09ee3b741aeb28b67fcf2751e0
Anthony Rizzo <anthony@moven.com>
2023-09-19 09:56:07 -0400

    WIP need to review

--- main.tf
+++ main.tf
@@ -1,20 +1,14 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 provider "aws" {
   region = var.region
-
   default_tags {
     tags = {
-      OWNER    = "RIZZO_A"
       CATEGORY = "ENG_ASSESSMENT"
+      OWNER    = "RIZZO_A"
     }
   }
-
 }
 
-# Filter out local zones, which are not currently supported 
-# with managed node groups
+# Filter out local zones, which are not currently supported  with managed node groups
 data "aws_availability_zones" "available" {
   filter {
     name   = "opt-in-status"
@@ -30,95 +24,4 @@ locals {
 resource "random_string" "suffix" {
   length  = 8
   special = false
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
-
-  name = "${local.cluster_name}-vpc"
-
-  cidr = "192.168.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  private_subnets = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
-  public_subnets  = ["192.168.11.0/24", "192.168.12.0/24", "192.168.13.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
-  }
-}
-
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_node_group#tracking-the-latest-eks-node-group-ami-releases
-data "aws_ssm_parameter" "eks_ami_release_version" {
-  name = "/aws/service/eks/optimized-ami/${local.cluster_version}/amazon-linux-2/recommended/release_version"
-}
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
-
-  cluster_name    = local.cluster_name
-  cluster_version = local.cluster_version
-
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
-
-  eks_managed_node_groups = {
-    one = {
-      name = "${local.cluster_name}-node-group-1"
-
-      instance_types = ["t3.small"] //c5.metal 
-
-      release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
-
-      min_size     = 3
-      max_size     = 6
-      desired_size = 3
-    }
-  }
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
-  }
-}
-
-resource "kubernetes_namespace" "rizzo" {
-  metadata {
-    name = "rizzo"
-  }
-}
-
-module "iam_eks_role" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-eks-role"
-
-  role_name = "${local.cluster_name}-cluster"
-
-  cluster_service_accounts = {
-    "${local.cluster_name}" = ["default:${local.cluster_name}-serviceaccount"]
-  }
 }
--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
-  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.7.0"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
     }
 
     random = {
@@ -17,11 +18,6 @@ terraform {
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0.4"
-    }
-
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
     }
   }
 
